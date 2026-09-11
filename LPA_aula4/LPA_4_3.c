eb23dbf86054865c31552312f4800e250718a3e3
Victoria M <victoriaq7@gmail.com>
2026-09-10 23:15:31 -0300

    Change while loop to do-while for input collection

--- LPA_aula4/LPA_4_3.c
+++ LPA_aula4/LPA_4_3.c
@@ -7,13 +7,13 @@ int main(void){
     float media = 0;
 
 
-    while(nota >= 0){
+    do{
         //solicita a nota
         printf("Insira sua nota: ");
         scanf("%f",&nota);
         soma = soma + nota;
         ct = ct + 1;
-    }
+    }while(nota >= 0);
 
     //calcula a media
     media = soma/ct;
